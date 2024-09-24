e84590ca537aa57fb81495e8e5776bb06e7c8787
irfan-007 <thanatos786687@gmail.com>
2024-09-24 20:56:52 +0530

    done 0

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,55 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [list, setList] = useState([]);
+  const [clicked, setclicked] = useState(false);
 
-export default App
+  useEffect(() => {
+    const data = fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => setList(json.data));
+  }, []);
+
+  return (
+    <div id="main" style={{ padding: "2rem" }}>
+      <h1>
+        <span>Fetch Users</span>{" "}
+        <button
+          id="btn"
+          style={{ float: "right" }}
+          onClick={() => setclicked(true)}
+        >
+          Get User List
+        </button>
+      </h1>
+      <table
+        style={{ backgroundColor: "wheat", width: "100%", padding: "0.5rem" }}
+      >
+        <thead style={{ backgroundColor: "#222", color: "white" }}>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clicked &&
+            list.map((item) => (
+              <tr key={item.id}>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+                <td>{item.email}</td>
+                <td>
+                  <img src={item.avatar} style={{}} />
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
